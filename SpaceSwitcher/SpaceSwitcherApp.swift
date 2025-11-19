96dc72dd481084b19292d864d4802197faab1873
Michael Y. Qiu <altmickymike@outlook.com>
2025-11-19 20:41:26 +0800

    Initial Commit

---
+++ SpaceSwitcher/SpaceSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceSwitcherApp.swift
+//  SpaceSwitcher
+//
+//  Created by Michael Qiu on 2025/11/19.
+//
+
+import SwiftUI
+
+@main
+struct SpaceSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
